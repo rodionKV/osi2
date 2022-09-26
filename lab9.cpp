4487cc38103156057b9c230b99848eb21de9bb72
r.kotov <r.kotov@g.nsu.ru>
2022-09-26 23:13:44 +0700

    add lab8 and lab9

--- lab9.cpp
+++ lab9.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <malloc.h>
+#include <signal.h>
 
 #define num_steps 200000000
 
@@ -13,27 +14,42 @@ struct info {
 struct result {
     double decimal;
 };
+bool stopcheck = false;
+pthread_barrier_t barrier1;
+pthread_barrier_t barrier2;
+
+static void sig_int(int signo) {
+    if (signo == SIGINT)
+        stopcheck = true;
+}
 
 void *writer(void *agr) {
     struct result *result = static_cast<struct result *>(malloc(sizeof(struct result)));
     //double pi_part = 0;
-    int i;
+     long long i;
     struct info structinfo = *static_cast<struct info *>(agr);
 
     result->decimal = 0;
-    // std::cout<<structinfo.first<<" "<<structinfo.size_step<<std::endl;
-    for (i = structinfo.first; i < num_steps; i += structinfo.size_step) {
 
+    for (i = structinfo.first;; i += structinfo.size_step) {
+        if (((i - structinfo.first) % 1000000) == 0) {
+            pthread_barrier_wait(&barrier1);
+            if (stopcheck) {
+                 pthread_barrier_wait(&barrier2);
+                pthread_exit((void *) result);
+            }
+        }
         result->decimal += 1.0 / (i * 4.0 + 1.0);
         result->decimal -= 1.0 / (i * 4.0 + 3.0);
     }
 
-    pthread_exit((void *) result);
+
 }
 
 int main(int argc, char **argv) {
     std::vector<pthread_t> threads;
-
+    pthread_barrier_init(&barrier1, nullptr, num_thread);
+    pthread_barrier_init(&barrier2, nullptr, num_thread);
     for (int j = 0; j < num_thread; ++j) {
         pthread_t pthread;
         auto *structinfo = static_cast<info *>(calloc(sizeof(struct info), 1));
@@ -51,11 +67,14 @@ int main(int argc, char **argv) {
         threads.push_back(pthread);
 
     }
+    if (signal(SIGINT, sig_int) == SIG_ERR) {
+        perror("невохможно перехватить сигнал");
+    }
     double result = 0;
     for (int j = 0; j < num_thread; ++j) {
         struct result *part_result;
 
-        pthread_join(threads[j], (void **)(&part_result));
+        pthread_join(threads[j], (void **) (&part_result));
 
         result += part_result->decimal;
     }
